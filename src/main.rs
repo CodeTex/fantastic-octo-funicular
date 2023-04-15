7aea72fa2487e72918b6e44c935b7787921b156e
CodeTex <29424456+CodeTex@users.noreply.github.com>
2023-04-15 22:53:51 +0200

    ADD test routes

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
 #[macro_use] extern crate rocket;
 
+use rocket::serde::json::Json;
+use rocket::serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Task {
+    status: String,
+    message: String,
+}
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/todo")]
+fn todo() -> Json<Task> {
+    let task = Task {
+        status: String::from("Success"),
+        message: String::from("Test message")
+    };
+    Json(task)
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index, todo])
 }
 
